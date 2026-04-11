73ea21546c1adf32328879cde1b5b1e63a5afbf7
jaevie <jonathanvuong4@yahoo.com>
2026-04-10 19:51:53 -0500

    initial commit

--- src/main/java/io/github/jvuong4/bloomfestal/BloomFestal.java
+++ src/main/java/io/github/jvuong4/bloomfestal/BloomFestal.java
@@ -1,15 +1,15 @@
-package io.github.username.modid;
+package io.github.jvuong4.bloomfestal;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModId implements ModInitializer {
-	public static final String ID = "mod_id";
+public class BloomFestal implements ModInitializer {
+	public static final String ID = "bloom_festal";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
 	public void onInitialize() {
-		//LOGGER.info("[Mod ID] pretty pink princess ponies prancing perpendicular");
+		LOGGER.info("[Bloom Festal] you are the ocean's BLOOM waves");
 	}
 }
